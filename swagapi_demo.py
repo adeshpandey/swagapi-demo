04bdf1a2346dbb98c35a0411042a1bd554fa5c10
adeshpandey <adesh.pandey10@gmail.com>
2019-09-22 13:15:58 +0530

    fixed app structure

--- swagapi_demo.py
+++
@@ -1,7 +0,0 @@
-import connexion
-
-# Create the application instance
-app = connexion.App(__name__, specification_dir='./')
-
-# Read the swagger.yml file to configure the endpoints
-app.add_api('swagger.yaml')
